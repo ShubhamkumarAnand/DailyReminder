b6c1b7b0125e897542102fafcb0309e7c1521e27
imskanand <the.anand3099@gmail.com>
2024-01-07 13:26:37 +0530

    Ui update in auth pages

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,11 @@
 import { SignIn } from "@clerk/nextjs";
 
 const SignInPage = () => {
-  return <SignIn />;
+  return (
+    <div className="flex items-center justify-center mt-12">
+      <SignIn />
+    </div>
+  );
 };
 
 export default SignInPage;
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
 
 const SignUpPage = () => {
-  return <SignUp />;
+  return (
+    <div className="flex items-center justify-center mt-12">
+      <SignUp />
+    </div>
+  );
 };
 
 export default SignUpPage;
